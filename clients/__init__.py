b2a2798e61cb02e6dd4a066be84cc522244ec5f9
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-05-03 04:23:13 +0700

    Add files via upload

---
+++ clients/__init__.py
@@ -0,0 +1,12 @@
+import asyncio
+from logs import logger
+
+# Setup event loop default
+try:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    logger.info("🚀 Asyncio event loop berhasil disiapkan secara global.")
+except Exception as e:
+    logger.error(f"❌ Gagal menyiapkan event loop: {e}")
+
+# Import client
+from .clients import BaseClient, Bot, UserBot, bot, navy
