e66937451bf6515f756fbe62999f3eae87a318ac
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-05-09 03:03:53 +0700

    Add callback functions for shop commands

---
+++ command/nokos_command.py
@@ -0,0 +1,19 @@
+from pyrogram import filters
+from pyrogram.types import CallbackQuery
+from pyrogram.handlers import CallbackQueryHandler
+from helpers.buttons import ButtonUtils
+
+
+async def cb_shop(_, callback: CallbackQuery):
+    text, button = await ButtonUtils.nokos(0)
+    return await callback.edit_message_text(
+          text, reply_markup=button
+    )
+
+
+async def cb_page_shop(_, callback: CallbackQuery):
+    page = int(callback.matches[0].group(1))
+    text, button = await ButtonUtils.nokos(page)
+    return await callback.edit_message_text(
+          text, reply_markup=button
+    ) 
