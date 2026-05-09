86e5f4a1480fdeeb150a15fc8e672838124b9bce
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-05-09 10:52:26 +0700

    Update buttons.py

--- helpers/buttons.py
+++ helpers/buttons.py
@@ -477,7 +477,7 @@ class ButtonUtils:
                     [
                         InlineKeyboardButton(
                             "Back",
-                            callback_data="back_menu"
+                            callback_data="open_nokos"
                         )
                     ]
                 ]
@@ -536,7 +536,7 @@ class ButtonUtils:
         nav_row.append(
             InlineKeyboardButton(
                 "Back",
-                callback_data="back_menu"
+                callback_data="open_nokos"
             )
         )
 
