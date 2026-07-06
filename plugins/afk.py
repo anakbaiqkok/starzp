4d24e13bdb297e8cdffe3482a0bd9122bf1eecc6
zpcode <zpkemem@gmail.com>
2026-07-06 14:21:49 +0800

    Update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -1,39 +1,51 @@
 from clients import star
-from command import REP_BLOCK
-from helpers import AFK_, CMD
+from helpers import CMD
+from helpers import AFK_, CMD, Emoji
 
-__MODULES__ = "AFK"
+__MODULES__ = "Afk"
 __HELP__ = """<blockquote>Command Help **AFK**</blockquote>
-<blockquote expandable>--**Basic Commands**--
 
+<blockquote>**Enabe afk mode**</blockquote>
     **You can set status to AFK mode**
         `{0}afk` (reason)
+    
+<blockquote>**Disable afk mode**</blockquote>
     **After AFK mode on, you can set to UNAFK mode**
         `{0}unafk`
-    **You can set mode private/all, if mode set to private afk message only working on private**
-        `{0}afk mode` (private/all)</blockquote>
 
 <b>   {1}</b>
 """
 
-IS_BASIC = True
-
 
 @CMD.NO_CMD("REP_BLOCK", star)
 async def _(client, message):
-    return await REP_BLOCK(client, message)
+    em = Emoji(client)
+    await em.get()
+    return await message.reply_text(
+        f"{em.block}**Ga usah reply apalagi tag gw, lu udah block gua anak KONTOL!!**"
+    )
 
 
 @CMD.UBOT("afk")
 async def _(client, message):
-    return await AFK_.set_afk(client, message)
+    emo = Emoji(client)
+    await emo.get()
+    rep = message.reply_to_message
+    if not rep:
+        return await message.reply(f"{emo.gagal}<b>Please reply to message!!</b>")
+    return await AFK_.set_afk(client, message, emo)
 
 
-@CMD.NO_CMD("AFK", star)
+@CMD.NO_CMD("AFK", zohun)
+@CMD.capture_err
 async def _(client, message):
-    return await AFK_.get_afk(client, message)
+    emo = Emoji(client)
+    await emo.get()
+    return await AFK_.get_afk(client, message, emo)
 
 
 @CMD.UBOT("unafk")
 async def _(client, message):
-    return await AFK_.unset_afk(client, message)
+    emo = Emoji(client)
+    await emo.get()
+    return await AFK_.unset_afk(client, message, emo)
