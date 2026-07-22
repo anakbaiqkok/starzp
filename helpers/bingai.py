b5e7a270bd84a0c7ae49e83527dc4c3136656364
zpcode <zpkemem@gmail.com>
2026-07-22 20:48:10 +0800

    Update bingai.py

--- helpers/bingai.py
+++ helpers/bingai.py
@@ -14,16 +14,14 @@ from logs import logger
 
 
 class Bing:
-    def __init__(self, cookies_file: str):
-        self.cookies = self._load_cookies(cookies_file)
+    def __init__(self, "data.json": str):
+        self.cookies = self._load_cookies("data.json")
         self.shuffled_cookies = self.cookies.copy()
         random.shuffle(self.shuffled_cookies)
         self.current_index = 0
 
-    def _load_cookies(self, cookies_file: str) -> List[Dict[str, str]]:
-        with open(cookies_file, "r", encoding="utf-8") as file:
-            # Tambahkan print ini sebelum baris 10
-            ###print(response.text) 
+    def _load_cookies(self, "data.json": str) -> List[Dict[str, str]]:
+        with open("data.json", "r", encoding="utf-8") as file:
             return json.load(file)
 
     def get_next_cookie(self) -> Optional[Dict[str, str]]:
@@ -43,8 +41,8 @@ class Bing:
         folder_name: str, prompt: str, max_global_retries: int = 5
     ):
         prompt_clean = re.sub(r"[^\x20-\x7E]", "", prompt.strip())
-        cookies_file = "storage/cookies/bing/bing.json"
-        gen = Bing(cookies_file)
+        "data.json" = "storage/cookies/bing/bing.json"
+        gen = Bing("data.json")
         retries = 0
 
         while retries < max_global_retries:
